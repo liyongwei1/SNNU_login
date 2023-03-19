4a52263e0f5763018c2017b6cdb013f52f24c14e
morninghut <morninghut@outlook.com>
2023-03-19 14:17:04 +0800

    Update SNNU_login.py
    似乎不需要构造 header 也可以正常使用？

--- SNNU_login.py
+++ SNNU_login.py
@@ -29,23 +29,6 @@ def Login(account, password):
     #获取cookie
     cookie = Get_cookie(post_addr)
 
-    #构造头部信息
-    post_header={
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7',
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Content-Length': '67',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': cookie,
-            'DNT': '1',
-            'Host': '202.117.144.205:8601',
-            'Origin': 'http://202.117.144.205:8601',
-            'Referer': 'http://202.117.144.205:8601/snnuportal/login.jsp',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'
-    }
 
     post_data = {
             'sourceurl': 'http://www.gstatic.com/generate_204',
@@ -54,7 +37,7 @@ def Login(account, password):
             'yys': '',
         }
 
-    response = requests.post(post_addr, data=post_data, headers=post_header)
+    response = requests.post(post_addr, data=post_data)
 
     return 'OK'
 
